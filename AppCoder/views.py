b682c5afbcc7819e846ba5d71b70f3f32dc67972
Mirko Gonzalez <mir_elmejor99@hotmail.com>
2023-03-20 23:15:57 -0300

    Guardado seguro

---
+++ AppCoder/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def inicio(request):
+    return render (request, "AppCoder/inicio.html")
+
+def cursos(request):
+    return render(request, "AppCoder/cursos.html")
+
+def profesores(request):
+    return render(request, "AppCoder/profesores.html")
+
+def estudiantes(request):
+    return render(request, "AppCoder/estudiantes.html")
+
+def entregables(request):
+    return render(request, "AppCoder/entregables.html")
